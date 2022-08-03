c36e3c795282c7a41ec883de0b4a9e9fc25c735f
Darktaste <vasil.lyubenov@gmail.com>
2022-08-03 23:04:44 +0300

    tasks 1 and 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,31 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    let i = 0;
+    let isFalse = false;
+
+    let result= "";
+
+    if(num < 0) {
+        isFalse = true;
+        
+    }
+
+    while(i < num) {
+
+        if(isFalse){
+            break;
+        }
+        
+        result += string;
+
+        i++;
+    }
+
+    if(isFalse) {
+        return "ERROR";
+    } else {
+        return result;
+    }
 
 };
 
