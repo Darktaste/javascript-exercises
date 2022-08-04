14d7e19427de9bdb6759bf3f3df02928725340b8
Darktaste <vasil.lyubenov@gmail.com>
2022-08-04 21:35:51 +0300

    sumAll task done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,37 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+
+    let result = 0;
+    let isTrue = true;
+
+    if (a < 0 || b < 0 || isNaN(a) || isNaN(b) || a === a.toString() || b === b.toString()) {
+
+        isTrue = false;
+
+    } else if (a <= b) {
+
+        for (let i = a; i <= b; i++) {
+            result += i;
+        }
+
+    } else if (a > b) {
+
+        for (let i = a; i >= b; i--) {
+            result += i;
+        }
+
+    }
+
+    if (isTrue) {
+        return result;
+    } else {
+        return "ERROR";
+    }
+
 
 };
+
+
+sumAll(-10, 4)
 
 // Do not edit below this line
 module.exports = sumAll;
