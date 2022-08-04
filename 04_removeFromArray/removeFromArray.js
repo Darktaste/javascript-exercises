f6ab302eb63a7b41b602239d2b91c80bb246c96a
Darktaste <vasil.lyubenov@gmail.com>
2022-08-04 21:05:54 +0300

    remove from array done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
-};
+    const arrayN = args[0];
+
+    let newArrey = [];
+
+    arrayN.forEach((element) => {
+
+        
+
+            if(!args.includes(element)){
+
+                newArrey.push(element)
+    
+            }
+       
+        
+        
+    });
+
+    return newArrey;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
