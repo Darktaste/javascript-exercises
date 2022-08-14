ebbec4156e695c7aec153a9f45ea2982b7db0972
Darktaste <vasil.lyubenov@gmail.com>
2022-08-14 13:53:22 +0300

    calculator task - done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let result = 0;
+	for(i = 0; i <= a.length-1; i++) {
+    result += Number(a[i]);
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let result = 1;
+	for(i = 0; i <= a.length-1; i++) {
+    result *= Number(a[i]);
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  for(let i = 1; i <= a; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
