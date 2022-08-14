0df5fcb0ab5780556b71be26e32edea833ec7ea0
Darktaste <vasil.lyubenov@gmail.com>
2022-08-14 14:32:32 +0300

    palindromes task done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = "";
+    let lowString = string.toLowerCase();
+    let replString = lowString.replace(/[^a-z0-9]/gi, "");
+    
+    for(let i = replString.length - 1; i >= 0; i--) {
+        newString += replString[i];
+    }
 
+    if(replString === newString) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+palindromes('A car, a man, a maraca.')
 // Do not edit below this line
 module.exports = palindromes;
