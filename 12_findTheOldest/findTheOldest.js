942faee1ac28ee3a4c15322b9eb0daf3d5475803
Darktaste <vasil.lyubenov@gmail.com>
2022-08-14 15:56:23 +0300

    findTheOldes task is done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((a ,b) => {
 
+        if(!a.yearOfDeath){
+            a.yearOfDeath = (new Date()).getFullYear();
+        } else if(!b.yearOfDeath) {
+            b.yearOfDeath = (new Date()).getFullYear();
+        }
+    
+        if((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+            return a;
+        } else if ((b.yearOfDeath - b.yearOfBirth) > (a.yearOfDeath - a.yearOfBirth)) {
+            return b;
+        }
+
+
+    });
 };
 
 // Do not edit below this line
