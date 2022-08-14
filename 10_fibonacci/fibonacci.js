f3e9b7f1a2eaaafbc1c484d75f02f35634b349c1
Darktaste <vasil.lyubenov@gmail.com>
2022-08-14 14:46:02 +0300

    fibonacci task done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let a = 0;
+    let b = 1;
+    let c = Number(n);
+    if(c < 0) {
+        return "OOPS";
+    }
+    
+    for(let i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
